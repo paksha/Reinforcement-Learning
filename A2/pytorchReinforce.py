9a5549d86972553c185852880d877617c4901a02
Akshay Patel <akpateln@amazon.com>
2020-07-30 21:30:55 -0400

    torch reinforce

--- A2/pytorchReinforce.py
+++ A2/pytorchReinforce.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
-
+import matplotlib.pyplot as plt
 from maze_env import Maze
 
 wall_shape=np.array([[6,3],[6,3],[6,2],[5,2],[4,2],[3,2],[3,3],
@@ -32,8 +32,8 @@ env = Maze(agentXY,goalXY,wall_shape, pits)
 class Policy(nn.Module):
     def __init__(self):
         super(Policy, self).__init__()
-        self.affine1 = nn.Linear(4, 128)
-        self.dropout = nn.Dropout(p=0.6)
+        self.affine1 = nn.Linear(100, 128)
+        self.dropout = nn.Dropout(p=0.5)
         self.affine2 = nn.Linear(128, 4)
 
         self.saved_log_probs = []
@@ -48,12 +48,19 @@ class Policy(nn.Module):
 
 
 policy = Policy()
-optimizer = optim.Adam(policy.parameters(), lr=1e-5)
+optimizer = optim.Adam(policy.parameters(), lr=1e-6)
 eps = np.finfo(np.float32).eps.item()
 
+def decode(state):
+    new_state = state[:2]
+    new_state = [int(new_state[0]-5)//40, int(new_state[1]-5)//40]
+    cell = 10*new_state[1]+new_state[0]
+    e = np.zeros(100)
+    e[cell] = 1
+    return e
 
 def select_action(state):
-    state = torch.from_numpy(np.array(state)).float().unsqueeze(0)
+    state = torch.from_numpy(np.array(decode(state))).float().unsqueeze(0)
     probs = policy(state)
     m = Categorical(probs)
     action = m.sample()
@@ -81,8 +88,9 @@ def finish_episode():
 
 
 def main():
-    batch_size = 5
     running_reward = 10
+    eps = []
+    points = []
     for i_episode in range(20000):
         state, ep_reward = env.reset(), 0
         for t in range(1, 10000):  # Don't infinite loop while learning
@@ -93,11 +101,15 @@ def main():
             policy.rewards.append(reward)
             ep_reward += reward
             if done:
+                eps.append(i_episode)
+                points.append(ep_reward)
+                if i_episode % 500 == 0:
+                    plt.plot(eps, points)
+                    plt.show()
                 break
 
         running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
-        if i_episode % batch_size == 0:
-            finish_episode()
+        finish_episode()
         if i_episode % args.log_interval == 0:
             print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
                   i_episode, ep_reward, running_reward))
